035d06c0c7cb71e1c28bcb952140d55858a89095
egokalp <erdi.gokalp@insparx.com>
2019-09-24 10:14:24 +0200

    added test translations

--- app/app-client/web/src/App.tsx
+++ app/app-client/web/src/App.tsx
@@ -9,6 +9,38 @@ import './styles/app.scss';
 library.add(fas, fab);
 
 const App: React.FC = () => {
+
+    fetch('/product', {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            'branchCode': 'ES_AR'
+        })
+    })
+    .then((res: any) => res.json())
+    .then((translations: Record<string, string>) => {
+        console.log('product config :', translations);
+    })
+    .catch((e: any) => {
+        console.log('product config request failed :', e);
+    });
+
+    fetch('/translation/ES_AR', {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'GET'
+    })
+    .then((res: any) => res.json())
+    .then((translations: Record<string, string>) => {
+        console.log('translation :', translations);
+    })
+    .catch((e: any) => {
+        console.log('translation request failed :', e);
+    });
+
   return (
     <div className="app">
         <Routes />
