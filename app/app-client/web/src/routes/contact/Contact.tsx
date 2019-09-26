254dd834c5c0bf64b235d9d36925e3887a2c55bc
egokalp <erdi.gokalp@insparx.com>
2019-09-26 17:24:28 +0200

    created favicon icon

--- app/app-client/web/src/routes/contact/Contact.tsx
+++ app/app-client/web/src/routes/contact/Contact.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import './contact.scss';
+import TranslateService from '../../services/Translate.service';
 
 const Contact: React.FC = () => {
     return (
         <div className="route-contact">
             <div className="location-img-wrapper">
-                <img className="location-img" src={'/images/forma/forma-location.jpg'} alt={'location'} />
+                <img className="location-img" src={'/images/forma/forma-location.jpg'} alt="location" />
             </div>
             <div className="contact-wrapper">
                 <div className="col contact-detail">
-                    <div className="contact-title">Showroom Palermo</div>
-                    <div className="contact-address-line">Humboldt 2190</div>
-                    <div className="contact-address-line">Palermo Hollywood CABA </div>
-                    <div className="contact-phone-number">Tel: +5411 4775 5717</div>
-                    <div className="contact-opening-hours">Horario de verano de lunes a viernes de 10:30hs a 19:00hs</div>
-                    <div className="contact-email">Email: forma@formamuebles.com</div>
+                    <div className="contact-title">{TranslateService.translate('forma.showroom.main')}</div>
+                    <div className="contact-address-line">{TranslateService.translate('forma.showroom.main.address')}</div>
+                    <div className="contact-phone-number">{TranslateService.translate('forma.commons.label.phone')}&nbsp;{TranslateService.translate('forma.showroom.mainoffice.phone')}</div>
+                    <div className="contact-opening-hours">{TranslateService.translate('forma.showroom.mainoffice.workinghours')}</div>
+                    <div className="contact-email">{TranslateService.translate('forma.common.label.email')}&nbsp;forma@formamuebles.com</div>
                 </div>
                 <div className="col contact-detail">
                     <div className="contact-title">Showroom San Isidro</div>
-                    <div className="contact-address-line">Gascon 206 oficina 3</div>
-                    <div className="contact-address-line">San Isidro</div>
+                    <div className="contact-address-line">Gascon 206 oficina 3 <br />San Isidro</div>
                     <div className="contact-phone-number">Tel: +5411 4700 2625</div>
-                    <div className="contact-opening-hours">Horario de verano de lunes a viernes de 10:30hs a 19:00hs los días sábados con cita previa</div>
-                    <div className="contact-email">Email: gufina@formamuebles.com</div>
+                    <div className="contact-opening-hours">{TranslateService.translate('forma.showroom.mainoffice.workinghours')}</div>
+                    <div className="contact-email">{TranslateService.translate('forma.common.label.email')}gufina@formamuebles.com</div>
                 </div>
             </div>
         </div>
